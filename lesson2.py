bda315aa8e39e2b97297754007d0bb4468b9ff6d
Назар Лакуста <n.lakusta@step.local>
2023-06-15 17:16:28 +0300

    створив другий файл там гра найбільше число

---
+++ lesson2.py
@@ -0,0 +1,12 @@
+num1 = 100
+num2 = 15
+num3 = 30
+
+if num1 > num2 and num1>num3:
+    print("number 1 is biggest")
+
+if num2 > num1 and num2>num3:
+    print("number 2 is biggest")
+
+if num3 > num2 and num3>num1:
+    print("number 3 is biggest")

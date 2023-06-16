ced6af1d4199175faf7fe8655bf7bff8986f2ebb
Назар Лакуста <n.lakusta@step.local>
2023-06-16 13:36:36 +0300

    урок 3

--- lesson2.py
+++ lesson2.py
@@ -1,12 +1,33 @@
-num1 = 100
-num2 = 15
-num3 = 30
+def checkIsAdult():
+    print(f"Ваш вік {age}")
+    if age >= 18:
+        print("Повнолотній")
+    else:
+        print("Не повнолітній")
 
-if num1 > num2 and num1>num3:
-    print("number 1 is biggest")
 
-if num2 > num1 and num2>num3:
-    print("number 2 is biggest")
+checkIsAdult()
+checkIsAdult()
+checkIsAdult()
+checkIsAdult()
+checkIsAdult()
+checkIsAdult()
+checkIsAdult()
+checkIsAdult()
+checkIsAdult()
+checkIsAdult()
 
-if num3 > num2 and num3>num1:
-    print("number 3 is biggest")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
